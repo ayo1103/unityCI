8ef93b3b73048f7dd67f6fbae86697d63799cc56
Ayo <ayo@metatheory.gg>
2022-09-20 18:39:07 +0800

    add build script

---
+++ Assets/Scripts/Editor/Builder.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class Builder
+{
+    [MenuItem("BuildTool/Build")]
+    public static void Build()
+    {
+        var buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] {"Assets/Scenes/SampleScene.unity"},
+            locationPathName = "D:/Workspace/UnityProjects/unityCI/Builds/Win64/Game.exe",
+            target = BuildTarget.StandaloneWindows64,
+            options = BuildOptions.None,
+        };
+
+        var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        var summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
