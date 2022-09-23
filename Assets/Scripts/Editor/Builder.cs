a29ef27e6556ecdfea693416d5e401bea3eb3e73
Ayo <ayo@metatheory.gg>
2022-09-23 21:25:24 +0800

    XD

--- Assets/Scripts/Editor/Builder.cs
+++ Assets/Scripts/Editor/Builder.cs
@@ -1,12 +1,37 @@
+using System;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 public class Builder
 {
+    private const string BuildPath_Win64_BuildVersion = "/Builds/Win64/StealthDuskGame.json";
+
+    [Serializable]
+    public class BuildVersion
+    {
+        public string version;
+    }
+
+    [MenuItem("BuildTool/GenerateVersionJson")]
+    public static void GenerateVersionJson()
+    {
+        var buildVersion = new BuildVersion
+        {
+            version = Application.version
+        };
+
+        string jsonPath = Directory.GetCurrentDirectory() + BuildPath_Win64_BuildVersion;
+        string contents = JsonUtility.ToJson(buildVersion);
+        File.WriteAllText(jsonPath, contents);
+    }
+
     [MenuItem("BuildTool/Build")]
     public static void Build()
     {
+        GenerateVersionJson();
+        
         var buildPlayerOptions = new BuildPlayerOptions
         {
             scenes = new[] {"Assets/Scenes/SampleScene.unity"},
